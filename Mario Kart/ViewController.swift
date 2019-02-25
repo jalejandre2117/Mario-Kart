86f5effd5298d0d0531d2398f4a41082812ea412
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2019-02-25 08:31:59 -0500

    Initial Commit

---
+++ Mario Kart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mario Kart
+//
+//  Created by Jorge Alejandre on 2/25/19.
+//  Copyright © 2019 Jorge Alejandre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
